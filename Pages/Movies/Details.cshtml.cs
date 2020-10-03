465c9ee9b3f40e621d591c3680d8ceba3619cae9
JackBro <jack.brosius@geneva.edu>
2020-10-03 12:25:30 -0400

    Added Database / Tables and got the interface working and got the logic created to automatically create the database / tables for you locally

---
+++ Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using YellowBucket1.Data;
+using YellowBucket1.Models;
+
+namespace YellowBucket1.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly YellowBucket1.Data.MoviesContext _context;
+
+        public DetailsModel(YellowBucket1.Data.MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public YellowBucket1.Models.Movies Movies { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movies = await _context.Movies.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movies == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/Movies/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using YellowBucket1.Data;
+using YellowBucket1.Models;
+
+namespace YellowBucket1.Pages.Movies
+{
+    public class EditModel : PageModel
+    {
+        private readonly YellowBucket1.Data.MoviesContext _context;
+
+        public EditModel(YellowBucket1.Data.MoviesContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public YellowBucket1.Models.Movies Movies { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movies = await _context.Movies.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movies == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Movies).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MoviesExists(Movies.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool MoviesExists(int id)
+        {
+            return _context.Movies.Any(e => e.ID == id);
+        }
+    }
+}
