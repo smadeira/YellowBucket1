2f80c389ba481cbd05c7d4a0dbe4fa24291c2b27
Steve Kessler <steven.kessler@geneva.edu>
2020-10-22 17:35:48 -0400

    Updated rentals/index page and created details of selected rentals

--- Pages/Movies/Rentals/Index.cshtml.cs
+++ Pages/Movies/Rentals/Index.cshtml.cs
@@ -4,14 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using YellowBucket1.Data;
+using YellowBucket1.Models;
 
 namespace YellowBucket1.Pages.Movies.Rentals
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly YellowBucket1.Data.MoviesContext _context;
 
+        public IndexModel(YellowBucket1.Data.MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<YellowBucket1.Models.Movies> Movies { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+       
+        public async Task OnGetAsync()
+        {
+            var movies = from m in _context.Movies
+                 select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+            Movies = await _context.Movies.ToListAsync();
         }
     }
 }
