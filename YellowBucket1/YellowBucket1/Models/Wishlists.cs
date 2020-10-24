f67a55a295998490a428e16b083f913c069b90f9
JackBro <jack.brosius@geneva.edu>
2020-10-24 14:13:09 -0400

    fixed history issues

---
+++ YellowBucket1/YellowBucket1/Models/Wishlists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace YellowBucket1.Models
+{
+#nullable enable
+    public class Wishlists
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public int CustomersID { get; set; }
+        [Required]
+        public int MoviesID { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+    }
+#nullable disable
+}
---
+++ YellowBucket1/YellowBucket1/PowershellCommand.ps1
@@ -0,0 +1,8 @@
+﻿dotnet tool install --global dotnet-ef
+dotnet tool update --global dotnet-ef
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet ef
+dotnet ef migrations list -v
+dotnet ef migrations add InitialCreate -v
+Write-Host "Press any key to continue ....."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
