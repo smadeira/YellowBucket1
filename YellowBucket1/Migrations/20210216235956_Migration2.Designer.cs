40b5f0de8e55923c1e3b9c1c0e9ecce4ab54b7bf
Steven Kessler <srkessle@geneva.edu>
2021-02-18 09:47:05 -0500

    added migration

--- YellowBucket1/Migrations/20210216235956_Migration2.Designer.cs
+++ YellowBucket1/Migrations/20210216235956_Migration2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YellowBucket1.Data;
 
 namespace YellowBucket1.Migrations
 {
     [DbContext(typeof(MoviesContext))]
-    partial class MoviesContextModelSnapshot : ModelSnapshot
+    [Migration("20210216235956_Migration2")]
+    partial class Migration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
