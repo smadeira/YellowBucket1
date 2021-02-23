d2939098577f6fac91d7b6968a39bb6163139e5c
Shadorain <shadow@Shadowraith.fios-router.home>
2021-02-23 18:37:33 -0500

    feat: removed powershell project integration, attempting to upgrade the entire project but that would involve alot more than I believe we need

--- YellowBucket1/Program.cs
+++ YellowBucket1/Program.cs
@@ -14,16 +14,11 @@ using Microsoft.EntityFrameworkCore;
 using System.Timers;
 using System.Threading;
 
-namespace YellowBucket1
-{
-    public class Program
-    {
+namespace YellowBucket1 {
+    public class Program {
         //private static System.Timers.Timer aTimer;
-
         
-        public static void Main(string[] args)
-        {
-
+        public static void Main(string[] args) {
             // MyProcess.Program();
             // CreateDbIfNotExists(host);
 
@@ -41,31 +36,13 @@ namespace YellowBucket1
             var host = CreateHostBuilder(args).Build();
             host.Run();
 
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             // var context = new MoviesContext(new DbContextOptions<MoviesContext>());
             // 
             //    var count = context.Movies.FromSqlRaw
             //   ("SELECT COUNT(*) FROM dbo.Movies");
-
         }
 
-        private static void SetTimer(string[] args)
-        {
+        private static void SetTimer(string[] args) {
             System.Timers.Timer aTimer;
         // Create a timer with a two second interval.
         aTimer = new System.Timers.Timer(240000);
@@ -75,68 +52,51 @@ namespace YellowBucket1
             aTimer.Enabled = true;
         }
 
-        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e) {
             Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
                               e.SignalTime);
-
-           
         }
 
-        private static void CreateDbIfNotExists(IHost host)
-        {
-            using (var scope = host.Services.CreateScope())
-            {
+        private static void CreateDbIfNotExists(IHost host) {
+            using (var scope = host.Services.CreateScope()) {
                 var services = scope.ServiceProvider;
-                try
-                {
+                try {
                     var context = services.GetRequiredService<MoviesContext>();
                     context.Database.EnsureCreated();
                     // DbInitializer.Initialize(context);
                 }
-                catch (Exception ex)
-                {
+                catch (Exception ex) {
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occurred creating the DB.");
                 }
             }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args) .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                 });
     }
 
-    class MyProcess
-    {
-        public static void Program()
-        {
-            try
-            {
-                using (Process myProcess = new Process())
-                {
-                    myProcess.StartInfo.UseShellExecute = true;
-                    // You can start any process, HelloWorld is a do-nothing example.
-                    myProcess.StartInfo.FileName = "Powershell.exe";
-                    myProcess.StartInfo.CreateNoWindow = false;
-                    myProcess.StartInfo.Arguments = $"-NoProfile -ExecutionPolicy unrestricted -file \"{"PowershellCommand.ps1"}\"";
+    class MyProcess {
+        public static void Program() {
+            /* try { */
+            /*     using (Process myProcess = new Process()) { */
+            /*         myProcess.StartInfo.UseShellExecute = true; */
+            /*         // You can start any process, HelloWorld is a do-nothing example. */
+            /*         myProcess.StartInfo.FileName = "Powershell.exe"; */
+            /*         myProcess.StartInfo.CreateNoWindow = false; */
+            /*         myProcess.StartInfo.Arguments = $"-NoProfile -ExecutionPolicy unrestricted -file \"{"PowershellCommand.ps1"}\""; */
 
-
-                    myProcess.Start();
-                    // This code assumes the process you are starting will terminate itself.
-                    // Given that is is started without a window so you cannot terminate it
-                    // on the desktop, it must terminate itself or you can do it programmatically
-                    // from this application using the Kill method.
-                    
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            /*         myProcess.Start(); */
+            /*         // This code assumes the process you are starting will terminate itself. */
+            /*         // Given that is is started without a window so you cannot terminate it */
+            /*         // on the desktop, it must terminate itself or you can do it programmatically */
+            /*         // from this application using the Kill method. */
+            /*     } */
+            /* } */
+            /* catch (Exception e) { */
+            /*     Console.WriteLine(e.Message); */
+            /* } */
         }
     }
 }
