cea3e0ea6ff7b8b7bed869f2f9d4d1b00e019009
andre <amstock@geneva.edu>
2021-03-02 19:45:43 -0500

    Broken stuff

---
+++ Migrations/20210303001622_migration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YellowBucket1.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_KiosksID",
+                table: "Inventories",
+                column: "KiosksID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inventories_Kiosks_KiosksID",
+                table: "Inventories",
+                column: "KiosksID",
+                principalTable: "Kiosks",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inventories_Kiosks_KiosksID",
+                table: "Inventories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Inventories_KiosksID",
+                table: "Inventories");
+        }
+    }
+}
