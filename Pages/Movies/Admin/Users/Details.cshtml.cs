956495814e044456f98386f51ae92d7fa70f693a
Steve Kessler <steven.kessler@geneva.edu>
2021-03-16 16:11:21 -0400

    Added admin page to view user accounts

---
+++ Pages/Movies/Admin/Users/Details.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using YellowBucket1.Data;
+using YellowBucket1.Models;
+
+namespace YellowBucket1
+{
+    public class UsersDetailsModel : PageModel
+    {
+        private readonly YellowBucket1.Data.MoviesContext _context;
+
+        public UsersDetailsModel(YellowBucket1.Data.MoviesContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Customers Users { get; set; }
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Users = await _context.Customers.FirstOrDefaultAsync(m => m.ID == id);
+            
+
+            if (Users == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Users).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExist(Users.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool UsersExist(int id)
+        {
+            return _context.Customers.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Pages/Movies/Admin/Users/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using YellowBucket1.Data;
+using YellowBucket1.Models;
+
+namespace YellowBucket1
+{
+    public class UserIndexModel : PageModel
+    {
+        private readonly YellowBucket1.Data.MoviesContext _context;
+
+        public UserIndexModel(YellowBucket1.Data.MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Customers> Users { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string UserSearch { get; set; }
+        public async Task OnGetAsync()
+        {
+            var usersName = from m in _context.Customers
+                         select m;
+            if (!string.IsNullOrEmpty(UserSearch))
+            {
+                usersName = usersName.Where(c => c.LasttName.Contains(UserSearch));
+            }
+            Users = await usersName.ToListAsync();
+
+        }
+    }
+}
